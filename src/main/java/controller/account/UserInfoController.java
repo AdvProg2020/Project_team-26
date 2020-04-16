a862cec33db0b71734a867603c8359869a683b4e
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-04-16 21:59:57 +0430

    Making it runnable

--- src/main/java/controller/account/UserInfoController.java
+++ src/main/java/controller/account/UserInfoController.java
@@ -3,29 +3,34 @@ package controller.account;
 public class UserInfoController {
 
     public String getEmail(String token) {
+        return null;
     }
 
     public void changeEmail(String email, String token) {
     }
 
     public String getUsername(String token) {
+        return null;
     }
 
     public void changeUsername(String username, String token) {
     }
 
     public String getPassword(String token) {
+        return null;
     }
 
     public void setPassword(String oldPassword, String newPassword, String token) {
     }
 
     public int getInfo(String key, String token) {
+        return 0;
     }
 
     public void changeInfo(String key, String value, String token) {
     }
 
     public String getRole(String token) {
+        return null;
     }
 }
