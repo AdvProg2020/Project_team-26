ab0683496007d13240df47aa7f3d4a13396bd736
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-05-10 23:53:47 +0430

    initialized CommentRepository.

--- src/main/java/model/repository/fake/FakeCommentRepository.java
+++ src/main/java/model/repository/fake/FakeCommentRepository.java
@@ -1,4 +1,43 @@
 package model.repository.fake;
 
-public class FakeCommentRepository {
+import model.Comment;
+import model.repository.CommentRepository;
+
+import java.util.List;
+
+public class FakeCommentRepository implements CommentRepository {
+
+    public FakeCommentRepository() {
+
+    }
+
+    @Override
+    public List<Comment> getAll() {
+        return null;
+    }
+
+    @Override
+    public Comment getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void save(Comment object) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void delete(Comment object) {
+
+    }
+
+    @Override
+    public boolean exist(int id) {
+        return false;
+    }
 }
