5c08e05719b21a9036d958b1b188da08a7a5ba8c
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-05-10 21:44:58 +0430

    Register Tests are Done.

--- src/test/java/controller/account/AuthenticationControllerTest.java
+++ src/test/java/controller/account/AuthenticationControllerTest.java
@@ -33,8 +33,24 @@ public class AuthenticationControllerTest {
 
     @Test
     public void registerTest() throws InvalidTokenException, InvalidFormatException, NoAccessException, InvalidAuthenticationException {
-        Exception ex = Assert.assertThrows(NoAccessException.class, () -> authenticationController.register(new Account("AryaRezaei","1234",Role.ADMIN),token));
+        /**Exception Tests**/
+        Exception ex = Assert.assertThrows(NoAccessException.class, () -> authenticationController.register(
+                new Account("AryaRezaei","1234",Role.ADMIN),token));
         Assert.assertEquals(ex.getMessage(),"You are not allowed to do that.");
+        ex = Assert.assertThrows(InvalidFormatException.class, () -> authenticationController.register(
+                new Account("Nigga"," ",Role.CUSTOMER),token));
+        Assert.assertEquals(ex.getMessage(),"Password format is incorrect.");
+        ex = Assert.assertThrows(InvalidFormatException.class, () -> authenticationController.register(
+                new Account("","1234",Role.CUSTOMER),token));
+        Assert.assertEquals(ex.getMessage(),"Username format is incorrect.");
+        ex = Assert.assertThrows(InvalidAuthenticationException.class, () -> authenticationController.register(
+                new Account("test1","1234",Role.CUSTOMER),token));
+        Assert.assertEquals(ex.getMessage(),"Username is already taken.");
+        /**End of Exception Tests**/
+        authenticationController.register(new Account("Arya","1234",Role.SELLER),token);
+        Assert.assertEquals("Arya",userRepository.getUserByName("Arya").getUsername());
+        authenticationController.register(new Account("Tataloo","124",Role.CUSTOMER),token);
+        Assert.assertEquals("Tataloo",userRepository.getUserByName("Tataloo").getUsername());
     }
 
     @Test
