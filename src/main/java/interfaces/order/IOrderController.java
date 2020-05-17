6852155065a40f3c5515d4c69c127d480884a152
Pouya Esmaili <esmaili.1380@gmail.com>
2020-05-17 16:29:47 +0430

    add todo and refactor

--- src/main/java/interfaces/order/IOrderController.java
+++ src/main/java/interfaces/order/IOrderController.java
@@ -8,14 +8,15 @@ import model.Order;
 import model.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IOrderController {
 
-    ArrayList<Order> getOrders(String token) throws NoAccessException, InvalidTokenException;
+    List<Order> getOrders(String token) throws NoAccessException, InvalidTokenException;
 
-    ArrayList<Order> getOrdersWithFilter(String sortField, boolean isAcsending, String token) throws NoAccessException, InvalidTokenException;
+    List<Order> getOrdersWithFilter(String sortField, boolean isAcsending, String token) throws NoAccessException, InvalidTokenException;
 
-    ArrayList<User> getProductBuyerByProductId(int productId, String token);//TODO
+    List<User> getProductBuyerByProductId(int productId, String token);//TODO
 
     Order getASingleOrder(int id, String token) throws NoAccessException, InvalidIdException, NoObjectIdException, InvalidTokenException;
 }
