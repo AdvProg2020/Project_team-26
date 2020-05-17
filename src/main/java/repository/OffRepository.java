465bf80d788aa55035103336f80250c28103b8a8
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-05-18 03:39:26 +0430

    added

--- src/main/java/repository/OffRepository.java
+++ src/main/java/repository/OffRepository.java
@@ -3,8 +3,10 @@ package repository;
 import model.Off;
 import model.OffItem;
 import model.OffRequest;
+import model.Product;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OffRepository extends Repository<Off> {
     void addRequest(Off off);
@@ -16,6 +18,8 @@ public interface OffRepository extends Repository<Off> {
     void acceptRequest(int requestId);
 
     void rejectRequest(int requestId);
+
+    public List<Product> getAllProductWithOff(Map<String, String> filter, String sortFiled, boolean isAscending);
 
     OffRequest getOffRequestById(int requestId);
 
