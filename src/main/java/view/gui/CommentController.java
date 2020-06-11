ce706ff0f8beee7c777035aed454d2ae6bb13f52
Pouya Esmaili <esmaili.1380@gmail.com>
2020-06-12 03:28:09 +0430

    add single comment to comment list

--- src/main/java/view/gui/CommentController.java
+++ src/main/java/view/gui/CommentController.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.VBox;
 import model.Comment;
 import model.Product;
 
+import java.io.IOException;
 import java.util.List;
 
 public class CommentController {
@@ -18,12 +19,14 @@ public class CommentController {
     public CommentController() {
     }
 
-    public void load(Product product) {
+    public void load(Product product) throws IOException {
+        commentBox.getChildren().removeAll();
         this.product = product;
         for (Comment comment: product.getComments()) {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/SingleComment.fxml"));
             SingleCommentController singleCommentController = (SingleCommentController) loader.getController();
             singleCommentController.load(comment);
+            commentBox.getChildren().add(loader.load());
         }
     }
 }
