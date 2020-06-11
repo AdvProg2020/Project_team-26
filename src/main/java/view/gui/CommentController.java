3ec7aadf0009079fe408d8034cce2a6d1eec1ee2
Pouya Esmaili <esmaili.1380@gmail.com>
2020-06-12 03:58:50 +0430

    add submit and separator to comment

--- src/main/java/view/gui/CommentController.java
+++ src/main/java/view/gui/CommentController.java
@@ -2,6 +2,7 @@ package view.gui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
 import model.Comment;
 import model.Product;
@@ -22,11 +23,15 @@ public class CommentController {
     public void load(Product product) throws IOException {
         commentBox.getChildren().removeAll();
         this.product = product;
+        Separator separator = null;
         for (Comment comment: product.getComments()) {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/SingleComment.fxml"));
             SingleCommentController singleCommentController = (SingleCommentController) loader.getController();
             singleCommentController.load(comment);
             commentBox.getChildren().add(loader.load());
+            separator = new Separator();
+            commentBox.getChildren().add(separator);
         }
+        commentBox.getChildren().remove(separator);
     }
 }
