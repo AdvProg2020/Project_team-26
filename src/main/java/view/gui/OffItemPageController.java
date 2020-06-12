caf005f9ec57bdb21cd76089f7f37fcfed027c94
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-06-13 03:01:50 +0430

    added off

---
+++ src/main/java/view/gui/OffItemPageController.java
@@ -0,0 +1,10 @@
+package view.gui;
+
+import java.io.IOException;
+
+public class OffItemPageController implements InitializableController {
+    @Override
+    public void initialize(int id) throws IOException {
+
+    }
+}
