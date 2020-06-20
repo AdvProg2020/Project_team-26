3a222389b312f842d4dc7d7916d5e10145550fdc
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-06-21 03:53:05 +0430

    sss

---
+++ src/main/java/view/gui/AdminOptionMenuController.java
@@ -0,0 +1,19 @@
+package view.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminOptionMenuController {
+
+    @FXML
+    private Button promoButton;
+    @FXML
+    private Button categoryButton;
+    @FXML
+    private Button usersButton;
+    @FXML
+    private Button newManagerButton;
+    @FXML
+    private Button requestsButton;
+
+}
