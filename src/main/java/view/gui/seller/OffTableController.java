202811d84e4f124a319e0fafa06df779820f9aa2
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-06-23 03:07:33 +0430

    off table finished

--- src/main/java/view/gui/seller/OffTableController.java
+++ src/main/java/view/gui/seller/OffTableController.java
@@ -11,11 +11,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.*;
 import view.gui.Constants;
 import view.gui.InitializableController;
 import view.gui.OrderItemController;
 import view.gui.PersonalInfoController;
+import view.gui.customer.OrderTableControllerForCustomer;
 
 import java.io.IOException;
 import java.util.*;
@@ -26,40 +28,47 @@ public class OffTableController implements InitializableController {
     @FXML
     private TableView offTableView;
     @FXML
-    private TableColumn<Date,Off> startDate;
+    private TableColumn<Date, Off> startDate;
     @FXML
-    private TableColumn<Date,Off> endDate;
+    private TableColumn<Date, Off> endDate;
     @FXML
-    private TableColumn<Status,Off> status;
+    private TableColumn<Status, Off> status;
 
     @Override
     public void initialize(int id) throws IOException {
+        //todo
+    }
 
-        offTableView.setOnMouseClicked(e->{
-            ObservableList<Off> itemsList =  offTableView.getSelectionModel().getSelectedItems();
+    private void loadSingleOff(Off off) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/OffPageForSeller.fxml"));
+        OffControllerPage offControllerPage = (OffControllerPage) loader.getController();
+        offControllerPage.initialize(off.getId());
+        offControllerPage.loadOffPage(off, this);
+        personalInfoController.updateAllBox(loader.load());
+    }
+
+    public void load(List<Off> offList, PersonalInfoController personalInfoController) {
+        this.personalInfoController = personalInfoController;
+        loadColumn(FXCollections.observableList(offList));
+    }
+
+    private void loadColumn(ObservableList<Off> observableList) {
+        startDate.setCellValueFactory(new PropertyValueFactory<>("startDate"));
+        endDate.setCellValueFactory(new PropertyValueFactory<>("endDate"));
+        status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        offTableView.setItems(observableList);
+        offTableView.setOnMouseClicked(e -> {
+            ObservableList<Off> objects = offTableView.getSelectionModel().getSelectedItems();
             try {
-                loadSingleOff(itemsList.get(itemsList.size()-1));
+                loadSingleOff(objects.get(objects.size() - 1));
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         });
     }
-    private void loadSingleOff(Off off) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/OffPageForSeller.fxml"));
-        OffControllerPage offControllerPage = (OffControllerPage) loader.getController();
-        offControllerPage.initialize(off.getId());
-        offControllerPage.loadOffPage(off,this);
-        personalInfoController.updateAllBox(loader.load());
-    }
-    public void load(List<Off> offList, PersonalInfoController personalInfoController){
-        //todo
 
 
-
-    }
-
-
-    public void deleteOffAndReloadBox(int offId){
+    public void deleteOffAndReloadBox(int offId) {
         personalInfoController.clearBox();
         try {
             offController.removeAOff(offId, Constants.manager.getToken());
@@ -76,11 +85,12 @@ public class OffTableController implements InitializableController {
 
 
     }
-    public void reloadTable(){
+
+    public void reloadTable() {
         List<Off> offs = null;
         try {
-            offs = offController.getAllOfForSellerWithFilter("date",true,0,100, Constants.manager.getToken());
-            load(offs,this.personalInfoController);
+            offs = offController.getAllOfForSellerWithFilter("date", true, 0, 100, Constants.manager.getToken());
+            load(offs, this.personalInfoController);
         } catch (NoAccessException e) {
             e.printStackTrace();
         } catch (InvalidTokenException e) {
