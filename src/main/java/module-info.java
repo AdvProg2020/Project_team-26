d0a1f9276d67e337aae9b7c52da341b3ad851cfe
Pouya Esmaili <esmaili.1380@gmail.com>
2020-06-22 22:35:42 +0430

    add necessary modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,13 @@
 open module Project {
+    requires java.base;
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.xml.bind;
+    requires net.bytebuddy;
     requires java.persistence;
+    requires java.sql;
     requires org.hibernate.orm.core;
     requires org.controlsfx.controls;
     requires java.naming;
