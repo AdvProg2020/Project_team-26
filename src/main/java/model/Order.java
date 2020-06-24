2c080310e3bf68d5976f16d0c003204372164b48
Pouya Esmaili <esmaili.1380@gmail.com>
2020-06-24 18:48:29 +0430

    define price for order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "`order`")
+@SecondaryTable(name = "order_price", pkJoinColumns = @PrimaryKeyJoinColumn(name = "order_id"))
 public class Order {
 
     @Id
@@ -20,9 +21,11 @@ public class Order {
     @Column(name = "date")
     private Date date;
 
-// TODO: define price for order
-//    private long totalPrice;
-//    private long paidAmount;
+    @Column(name = "total_price", table = "order_price", insertable = false, updatable = false)
+    private Long totalPrice;
+
+    @Column(name = "paid_amount", table = "order_price", insertable = false, updatable = false)
+    private Long paidAmount;
 
     @ManyToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "user_id")
@@ -34,6 +37,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "promo_id")
     private Promo usedPromo;
+
+    @Column(name = "discount")
+    private long discount;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> items;
@@ -59,18 +65,20 @@ public class Order {
 
     public Order(Order order, Seller seller) {
         this.date = order.date;
-//        this.paidAmount = 0;
         this.usedPromo = null;
         for (OrderItem item : order.items) {
-            if(item.getSeller().getUsername().equals(seller.getUsername())) {
+            if (item.getSeller().getUsername().equals(seller.getUsername())) {
                 items.add(item);
-//                totalPrice += item.getPrice();
             }
         }
     }
 
     public Promo getUsedPromo() {
         return usedPromo;
+    }
+
+    public long getDiscount() {
+        return discount;
     }
 
     public List<OrderItem> getItems() {
@@ -85,18 +93,42 @@ public class Order {
         items.add(orderItem);
     }
 
-    public long getTotalPrice() {
+    public long calculateTotalPrice() {
         return items.stream().
                 map(item -> item.getPrice()).
                 reduce((price, totalPrice) -> price + totalPrice).
                 orElse((long) 0);
     }
 
-    public long getPaidAmount() {
+    public long calculatePaidAmount() {
         return items.stream().
                 map(item -> item.getPaidPrice()).
                 reduce((price, paidAmount) -> price + paidAmount).
                 orElse((long) 0);
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Long getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setDiscount() {
+        discount = calculateDiscount();
+    }
+
+    public long calculateDiscount() {
+        if (usedPromo != null) {
+            long paid = calculatePaidAmount();
+            if ((long) (paid * usedPromo.getPercent() / 100) > usedPromo.getMaxDiscount()) {
+                return usedPromo.getMaxDiscount();
+            } else {
+                return (long) (paid * usedPromo.getPercent() / 100);
+            }
+        }
+        return  0;
     }
 
     public Date getDate() {
@@ -107,7 +139,7 @@ public class Order {
         this.customer = customer;
     }
 
-    public String getAddress(){
+    public String getAddress() {
         return address;
     }
 }
