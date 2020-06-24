f0a1cc9a6ba04a0fbe129d2836fbec3221418c3f
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-06-24 21:47:02 +0430

    90 percent

--- src/main/java/view/gui/admin/AdminOptionMenuController.java
+++ src/main/java/view/gui/admin/AdminOptionMenuController.java
@@ -1,9 +1,24 @@
 package view.gui.admin;
 
+import controller.interfaces.account.IShowUserController;
+import controller.interfaces.category.ICategoryController;
+import exception.InvalidIdException;
+import exception.InvalidTokenException;
+import exception.NoAccessException;
+import exception.NotLoggedINException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import model.Admin;
+import view.cli.ControllerContainer;
+import view.gui.CategoryListController;
+import view.gui.Constants;
+import view.gui.customer.OrderTableController;
 import view.gui.interfaces.*;
-public class AdminOptionMenuController {
+
+import java.io.IOException;
+
+public class AdminOptionMenuController implements InitializableController {
 
     @FXML
     private Button promoButton;
@@ -15,5 +30,86 @@ public class AdminOptionMenuController {
     private Button newManagerButton;
     @FXML
     private Button requestsButton;
+    @FXML
+    private Button personalPage;
 
+    private int userId;
+    private Admin admin;
+    private IShowUserController showUserController;
+    private ICategoryController categoryController;
+
+
+    @Override
+    public void initialize(int id) throws IOException, InvalidTokenException, NoAccessException {
+        showUserController = (IShowUserController) Constants.manager.getControllerContainer().getController(ControllerContainer.Controller.ShowUserController);
+        categoryController = (ICategoryController) Constants.manager.getControllerContainer().getController(ControllerContainer.Controller.CategoryController);
+        this.userId = id;
+        this.admin = (Admin) showUserController.getUserById(id,Constants.manager.getToken());
+        categoryButton.setOnMouseClicked(e -> {
+            try {
+                handleCategory();
+            } catch (IOException | InvalidIdException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        promoButton.setOnMouseClicked(e -> {
+            try {
+                handlePromo();
+            } catch (InvalidTokenException invalidTokenException) {
+                invalidTokenException.printStackTrace();
+            } catch (NoAccessException noAccessException) {
+                noAccessException.printStackTrace();
+            }
+        });
+        personalPage.setOnMouseClicked(e -> {
+            try {
+                handlePersonalPage();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+
+        requestsButton.setOnMouseClicked(e -> {
+            try {
+                handleRequest();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (InvalidTokenException invalidTokenException) {
+                invalidTokenException.printStackTrace();
+            } catch (NoAccessException noAccessException) {
+                noAccessException.printStackTrace();
+            } catch (NotLoggedINException notLoggedINException) {
+                notLoggedINException.printStackTrace();
+            }
+        });
+    }
+
+    private void handleRequest() throws NoAccessException, InvalidTokenException, IOException, NotLoggedINException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/ManagerRequest.fxml"));
+        ManagerRequestController managerRequestController = loader.getController();
+        managerRequestController.initialize(2);
+        managerRequestController.load();
+    }
+
+    private void handlePersonalPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/AdminMenu.fxml"));
+        AdminMenuController adminMenuController = loader.getController();
+        adminMenuController.initialize(admin.getId());
+        adminMenuController.load();
+    }
+
+    private void handleCategory() throws IOException, InvalidIdException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/CategoryList.fxml"));
+            CategoryListController categoryListController = (CategoryListController) loader.getController();
+            categoryListController.initialize(1);
+            categoryListController.load(categoryController.getCategory(1,Constants.manager.getToken()));
+    }
+
+    private void handlePromo() throws NoAccessException, InvalidTokenException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/PromoManager.fxml")); {
+            PromoManagerController promoManagerController = (PromoManagerController) loader.getController();
+            promoManagerController.initialize(1);
+            promoManagerController.load();
+        }
+    }
 }
--- src/main/java/view/gui/admin/PromoManagerController.java
+++ src/main/java/view/gui/admin/PromoManagerController.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class PromoManagerController {
+public class PromoManagerController implements InitializableController {
 
 
     @FXML
@@ -47,18 +47,26 @@ public class PromoManagerController {
     private IPromoController controller;
 
 
-    private void load() throws InvalidTokenException, NoAccessException, NotLoggedINException {
+    public void initialize(int id) throws InvalidTokenException, NoAccessException {
         ObservableList list;
         controller = (IPromoController) Constants.manager.getControllerContainer().getController(ControllerContainer.Controller.PromoController);
-        allPromos = controller.getAllPromoCodeForCustomer(null,true,0,0,Constants.manager.getToken());
-        list = FXCollections.observableList(allPromos);
-        promoTable.setItems(list);
-        ObservableList<TableColumn> cols = promoTable.getColumns();
-        cols.get(0).setCellValueFactory(new PropertyValueFactory<Promo, Date>("startDate"));
-        cols.get(1).setCellValueFactory(new PropertyValueFactory<Promo,Date>("endDate"));
-        cols.get(2).setCellValueFactory(new PropertyValueFactory<Promo,String>("promoCode"));
-        cols.get(3).setCellValueFactory(new PropertyValueFactory<Promo,Double>("percent"));
-        cols.get(4).setCellValueFactory(new PropertyValueFactory<Promo,Long>("maxDiscount"));
+        try {
+            allPromos = controller.getAllPromoCodeForCustomer(null,true,0,0,Constants.manager.getToken());
+            list = FXCollections.observableList(allPromos);
+            promoTable.setItems(list);
+            ObservableList<TableColumn> cols = promoTable.getColumns();
+            cols.get(0).setCellValueFactory(new PropertyValueFactory<Promo, Date>("startDate"));
+            cols.get(1).setCellValueFactory(new PropertyValueFactory<Promo,Date>("endDate"));
+            cols.get(2).setCellValueFactory(new PropertyValueFactory<Promo,String>("promoCode"));
+            cols.get(3).setCellValueFactory(new PropertyValueFactory<Promo,Double>("percent"));
+            cols.get(4).setCellValueFactory(new PropertyValueFactory<Promo,Long>("maxDiscount"));
+        } catch (NotLoggedINException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+
     }
 
     @FXML
