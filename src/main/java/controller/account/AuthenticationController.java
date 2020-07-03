d9170e5599a4f810ebdc09f011fb60c72aa4fe6c
nothatcreative5 <59166376+nothatcreative5@users.noreply.github.com>
2020-07-03 16:02:40 +0430

    second done.

--- src/main/java/controller/account/AuthenticationController.java
+++ src/main/java/controller/account/AuthenticationController.java
@@ -152,8 +152,6 @@ public class AuthenticationController implements IAuthenticationController {
     }
 
     private void registerSeller(Account account, String token) {
-        User test = createNewUser(account);
-        System.out.println(test.getCompanyName());
         userRepository.save(createNewUser(account));
     }
 
