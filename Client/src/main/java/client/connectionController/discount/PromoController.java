51da0a953f2988fafa0de8fc232ced74df04b83c
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-07-19 00:59:08 +0430

    account is corrected

--- Client/src/main/java/client/connectionController/discount/PromoController.java
+++ Client/src/main/java/client/connectionController/discount/PromoController.java
@@ -1,15 +1,32 @@
 package client.connectionController.discount;
 
-
 import client.connectionController.interfaces.discount.IPromoController;
 import client.exception.*;
+import client.gui.Constants;
 import client.model.*;
-import client.model.enums.Role;
+import client.model.enums.*;
+import net.minidev.json.JSONObject;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Date;
 import java.util.List;
 
 public class PromoController implements IPromoController {
+
+    public String getRandomPromoForUserSet(String token) throws InvalidTokenException {
+        Session session = Session.getSession(token);
+        if (session.getLoggedInUser() == null)
+            return null;
+        if (session.getLoggedInUser().getRole() != Role.CUSTOMER)
+            return null;
+        Pair<Boolean, String> promo = session.getPromoCodeForUser();
+        if (promo.getKey() == false)
+            return null;
+        if (promo.getValue() == "")
+            return null;
+        session.setPromoCodeForUser(new Pair<>(false, ""));
+        return promo.getValue();
+    }
 
     public Promo getPromoCodeTemplateByCode(String codeId, String token) throws InvalidIdException, NotLoggedINException {
         Promo promo = promoRepository.getByCode(codeId);
@@ -49,119 +66,77 @@ public class PromoController implements IPromoController {
         return promo.getId();
     }
 
-    private void checkAccessOfUser(String token, String message) throws NoAccessException, InvalidTokenException, NotLoggedINException {
-        User user = Session.getSession(token).getLoggedInUser();
-        if (user == null) {
-            throw new NotLoggedINException("You are not logged in.");
-        } else if (user.getRole() != Role.ADMIN) {
-            throw new NoAccessException(message);
-        }
-    }
-
-    private Pageable createAPage(String sortField, boolean isAscending, int startIndex, int endIndex) {
-        if (isAscending) {
-            return new Pageable(startIndex, endIndex, sortField, Pageable.Direction.ASCENDING);
-        } else {
-            return new Pageable(startIndex, endIndex, sortField, Pageable.Direction.DESCENDING);
-        }
-    }
-
-    /**
-     * 2 function below should be checked
-     */
-
     public void removePromoCode(int promoCodeId, String token) throws NotLoggedINException, NoAccessException, InvalidIdException, InvalidTokenException, NoObjectIdException {
-        checkAccessOfUser(token, "only manager can remove the promo");
-        Promo promo = promoRepository.getById(promoCodeId);
-        if (promo == null)
-            throw new InvalidIdException("there is no promo by " + promoCodeId);
-        promoRepository.delete(promo);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoCodeId", promoCodeId);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
+        }
     }
 
     public void addCustomer(int promoId, int customerId, String token) throws NoAccessException, InvalidIdException, ObjectAlreadyExistException, InvalidTokenException, NotLoggedINException, NotCustomerException {
-        checkAccessOfUser(token, "only the manager can add customer");
-        Promo promo = getPromoByIdWithCheck(promoId);
-        User user = userRepository.getById(customerId);
-
-        if (user == null) {
-            throw new InvalidIdException("no customer exists By " + customerId + " id");
-        } else if (user.getRole() != Role.CUSTOMER) {
-            throw new NotCustomerException("Only Customers can have promo codes");
-        } else if (promo.getCustomers().contains((Customer) user)) {
-            throw new ObjectAlreadyExistException("the promo contain this customer", (Customer)
-                    user);
-        } else {
-            promo.getCustomers().add((Customer) user);
-            promoRepository.save(promo);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoId", promoId);
+        jsonObject.put("customerId", customerId);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
         }
 
     }
 
     public void removeCustomer(int promoId, int customerId, String token) throws NoAccessException, InvalidIdException, InvalidTokenException, NotLoggedINException, NotCustomerException {
-        checkAccessOfUser(token, "only the manager can remove customer");
-        Promo promo = getPromoByIdWithCheck(promoId);
-        User user = userRepository.getById(customerId);
-        if (user == null) {
-            throw new InvalidIdException("no customer exist By " + customerId + " id");
-        } else if (user.getRole() != Role.CUSTOMER) {
-            throw new NotCustomerException("You must choose a customer");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoId", promoId);
+        jsonObject.put("customerId", customerId);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
         }
-        Customer customer = (Customer) user;
-        List<Customer> promos = promo.getCustomers();
-
-        if (!promos.contains(customer))
-            throw new InvalidIdException("the promo doesnt contain " + customerId + " id");
-
-        promos.remove(customer);
-        promoRepository.save(promo);
     }
 
     public void setPercent(int promoId, double percent, String token) throws InvalidIdException, NoAccessException, InvalidTokenException, InvalidDiscountPercentException, NotLoggedINException {
-        checkAccessOfUser(token, "only manager can set percent");
-        Promo promo = getPromoByIdWithCheck(promoId);
-        if (percent > 100.0)
-            throw new InvalidDiscountPercentException("the percent can't exceed 100%");
-        promo.setPercent(percent);
-        promoRepository.save(promo);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoId", promoId);
+        jsonObject.put("percent", percent);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
+        }
     }
 
     public void setMaxDiscount(int promoId, long maxDiscount, String token) throws NoAccessException, InvalidIdException, InvalidTokenException, NotLoggedINException {
-        checkAccessOfUser(token, "only manager can set percent");
-        Promo promo = getPromoByIdWithCheck(promoId);
-        promo.setMaxDiscount(maxDiscount);
-        promoRepository.save(promo);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoId", promoId);
+        jsonObject.put("maxDiscount", maxDiscount);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
+        }
     }
 
-    private Promo getPromoByIdWithCheck(int id) throws InvalidIdException {
-        Promo promo = (Promo) promoRepository.getById(id);
-        if (promo == null)
-            throw new InvalidIdException("no promo exist");
-        return promo;
-    }
 
     public void setTime(int promoId, Date date, String type, String token) throws NoAccessException, InvalidIdException, InvalidTokenException, NotLoggedINException {
-        checkAccessOfUser(token, "only manager can " + type + " date");
-        Promo promo = getPromoByIdWithCheck(promoId);
-        if (type.equals("start")) {
-            promo.setStartDate(date);
-        } else {
-            promo.setEndDate(date);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("promoId", promoId);
+        jsonObject.put("date", date);
+        jsonObject.put("type", type);
+        jsonObject.put("token", token);
+        try {
+            Constants.manager.postRequestWithVoidReturnType(jsonObject, Constants.getOffControllerAddProductToOffAddress());
+        } catch (HttpClientErrorException e) {
+            throw new InvalidTokenException("ksamd");
         }
-        promoRepository.save(promo);
-    }
-
-    public String getRandomPromoForUserSet(String token) throws InvalidTokenException {
-        Session session = Session.getSession(token);
-        if (session.getLoggedInUser() == null)
-            return null;
-        if (session.getLoggedInUser().getRole() != Role.CUSTOMER)
-            return null;
-        Pair<Boolean, String> promo = session.getPromoCodeForUser();
-        if (promo.getKey() == false)
-            return null;
-        if (promo.getValue() == "")
-            return null;
-        session.setPromoCodeForUser(new Pair<>(false, ""));
-        return promo.getValue();
     }
 }
