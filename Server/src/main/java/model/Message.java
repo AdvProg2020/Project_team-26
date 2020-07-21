338bc539c973e012a60e7b97982014b89673d22b
Pouya Esmaili <esmaili.1380@gmail.com>
2020-07-21 15:50:54 +0430

    add message to model

---
+++ Server/src/main/java/model/Message.java
@@ -0,0 +1,44 @@
+package model;
+
+import model.enums.MessageType;
+
+import java.util.Date;
+
+public class Message {
+    private User sender;
+    private String content;
+    private MessageType type;
+    private Date time;
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
---
+++ Server/src/main/java/model/enums/MessageType.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum MessageType {
+    JOIN, CHAT, LEAVE
+}
