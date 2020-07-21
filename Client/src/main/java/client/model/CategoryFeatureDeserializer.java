5c443f903e479bd5247803a684624f89f0b862fb
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-07-21 07:12:43 +0430

    wtf

--- Client/src/main/java/client/model/CategoryFeatureDeserializer.java
+++ Client/src/main/java/client/model/CategoryFeatureDeserializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.jackson.JsonComponent;
 import java.io.IOException;
 
@@ -16,7 +17,7 @@ public class CategoryFeatureDeserializer extends JsonDeserializer<CategoryFeatur
     public CategoryFeature deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         JsonNode node = jp.getCodec().readTree(jp);
         int id = (int) ((IntNode) node.get("id")).numberValue();
-        String featureName = node.get("featureName").asText();
+        String featureName = ((ObjectNode)node.get("featureName")).asText();
         FeatureType featureType = makeFeatureType(node.get("featureType").asText());
         return new CategoryFeature(id, featureName, featureType);
     }
