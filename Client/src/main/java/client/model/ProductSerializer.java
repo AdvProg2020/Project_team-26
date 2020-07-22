cfa0c2cf672d6b4ab38913cc7aa23a0197f817e1
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-07-23 02:35:30 +0430

    plzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz

--- Client/src/main/java/client/model/ProductSerializer.java
+++ Client/src/main/java/client/model/ProductSerializer.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class ProductSerializer extends StdSerializer<Product> {
 
     public ProductSerializer() {
-        super(Map.class, true);
+        super(Product.class, true);
     }
 
     @Override
